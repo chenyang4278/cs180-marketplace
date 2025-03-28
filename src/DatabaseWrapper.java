02ba9ceabf4b4d2d4d43f298f8fa6fb6d95fca52
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2025-03-28 12:35:32 -0400

    uncomment update line

--- src/DatabaseWrapper.java
+++ src/DatabaseWrapper.java
@@ -153,7 +153,7 @@ public class DatabaseWrapper implements IDatabaseWrapper {
             }
 
             // update entire row here (method doesn't exist yet)
-            // db.updateRow("id", String.valueOf(obj.getId()), obj.asRow());
+            db.update("id", String.valueOf(obj.getId()), obj.asRow());
         } catch (DatabaseNotFoundException e) {
             throw new DatabaseWriteException("Failed to save");
         }
