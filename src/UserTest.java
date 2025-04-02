3964e84406b93a443afc67615a13f06d0dc0590a
Chen Yang <chenyang4278@gmail.com>
2025-04-01 21:50:04 -0400

    adding more test methods

--- src/UserTest.java
+++ src/UserTest.java
@@ -14,5 +14,40 @@ public class UserTest {
     }
 
     @Test
+    public void testSettersAndGetters(){
+        User user = new User("dawn_xin", "mypassword");
+
+        user.setUsername("new_username");
+        assertEquals("new_username", user.getUsername());
+
+        user.setPassword("newpassword123");
+        assertEquals("newpassword123", user.getPassword());
+    }
+
+    @Test
+    public void testUpdateBalance() {
+        User user = new User("ez", "securepass");
+
+        assertEquals(0.0, user.getBalance(), 0.01);
+
+        user.updateBalance(100.0);
+        assertEquals(100.0, user.getBalance(), 0.01);
+
+        user.updateBalance(-50.0);
+        assertEquals(50.0, user.getBalance(), 0.01);
+    }
+
+    @Test
+    public void testUpdateRating() {
+        User user = new User("hello", "password");
+
+        assertEquals(0.0, user.getRating(), 0.01);
+
+        user.updateRating(4.5);
+        assertEquals(4.5, user.getRating(), 0.01);
+
+        user.updateRating(5.0);
+        assertEquals(5.0, user.getRating(), 0.01);
+    }
 
 }
