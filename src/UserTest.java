bbff04c182951793c0ef59a16dbd5f02fbd08f15
Chen Yang <chenyang4278@gmail.com>
2025-04-01 21:47:12 -0400

    nvm wasn't able to add junit to classpath but writing junit tests anyway

--- src/UserTest.java
+++ src/UserTest.java
@@ -4,5 +4,15 @@ import static org.junit.Assert.*;
 public class UserTest {
 
     @Test
+    public void testUserCreation(){
+        User user  = new User("chen_yang","passwordabc");
+        assertNotNull(user);
+        assertEquals("chen_yang",user.getUsername());
+        assertEquals("passwordabc", user.getPassword());
+        assertEquals(0.0, user.getBalance(), 0.01);
+        assertEquals(0.0, user.getRating(), 0.01);
+    }
+
+    @Test
 
 }
