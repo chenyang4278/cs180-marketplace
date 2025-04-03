dc63d29185819a69f2dd57a846222c903ae6544a
Sheppsu <49356627+Sheppsu@users.noreply.github.com>
2025-04-03 02:38:25 -0400

    add tests for Serializable

---
+++ src/SerializableTest.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SerializableTest {
+    @Test
+    public void testGetColumns() {
+        assertArrayEquals(
+            new String[]{"id", "name", "count", "long_count", "decimal", "precise_decimal"},
+            Serializable.getColumns(TestTable.class)
+        );
+    }
+
+    @Test
+    public void testAsRow() {
+        TestTable t = new TestTable("table1", 10, 30, 5.4f, 3.2d);
+        assertArrayEquals(
+            new String[] {String.valueOf(t.getId()), "table1", "10", "30", "5.4", "3.2"},
+            t.asRow()
+        );
+    }
+
+    @Test
+    public void testFromRow() {
+        TestTable t = Serializable.fromRow(
+            TestTable.class,
+            new String[] {"1", "table1", "10", "30", "5.4", "3.2"}
+        );
+        assertEquals(1, t.getId());
+        assertEquals("table1", t.getName());
+        assertEquals(10, t.getCount());
+        assertEquals(30, t.getLongCount());
+        assertEquals(5.4, t.getDecimal(), 0.001);
+        assertEquals(3.2, t.getPreciseDecimal(), 0.001);
+    }
+
+    @Test
+    public void testToAndBack() {
+        TestTable t1 = new TestTable("table1", 10, 30, 5.4f, 3.2d);
+        TestTable t2 = Serializable.fromRow(TestTable.class, t1.asRow());
+        assertTrue(t1.equals(t2));
+    }
+}
---
+++ src/TestTable.java
@@ -0,0 +1,80 @@
+public class TestTable extends Serializable {
+    @SerializableField( field = "name", index = 1 )
+    private String name;
+
+    @SerializableField( field = "count", index = 2 )
+    private int count;
+
+    @SerializableField( field = "long_count", index = 3 )
+    private long longCount;
+
+    @SerializableField( field = "decimal", index = 4 )
+    private float decimal;
+
+    @SerializableField( field = "precise_decimal", index = 5 )
+    private double preciseDecimal;
+
+    public TestTable() {}
+
+    public TestTable(String name, int count, long longCount, float decimal, double preciseDecimal) {
+        this.name = name;
+        this.count = count;
+        this.longCount = longCount;
+        this.decimal = decimal;
+        this.preciseDecimal = preciseDecimal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public long getLongCount() {
+        return longCount;
+    }
+
+    public void setLongCount(long longCount) {
+        this.longCount = longCount;
+    }
+
+    public float getDecimal() {
+        return decimal;
+    }
+
+    public void setDecimal(float decimal) {
+        this.decimal = decimal;
+    }
+
+    public double getPreciseDecimal() {
+        return preciseDecimal;
+    }
+
+    public void setPreciseDecimal(double preciseDecimal) {
+        this.preciseDecimal = preciseDecimal;
+    }
+
+    public boolean equals(TestTable other) {
+        return (
+            name.equals(other.name) &&
+                count == other.count &&
+                longCount == other.longCount &&
+                decimal == other.decimal &&
+                preciseDecimal == other.preciseDecimal
+        );
+    }
+
+    public static TestTable getById(int id) throws RowNotFoundException {
+        return DatabaseWrapper.get().getById(TestTable.class, id);
+    }
+}
