72aef508ebbbd4a64a65d5e3823c5bce1f0aa08c
Chen Yang <chenyang4278@gmail.com>
2025-04-05 19:17:56 -0400

    added javadocs

--- src/IUser.java
+++ src/IUser.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
-
+/**
+ * IUser
+ * <p>
+ * interface for user class
+ *
+ * @author Chen Yang, section 24
+ * @version 3/28/25
+ */
 public interface IUser {
     String getUsername();
 
