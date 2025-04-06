71ddb491b478205fa842e409fa541630b6cedffe
asiandedeye <bennyh1005@gmail.com>
2025-04-06 06:19:41 -0400

    Update IListing.java

--- src/IListing.java
+++ src/IListing.java
@@ -1,3 +1,17 @@
+/**
+ *
+ * IListing.java
+ *
+ * This interface creates a
+ * list of methods that are
+ * used by the listing class.
+ * 
+ * @author Benny Huang, sec 024
+ *
+ * @version April 06, 2025
+ *
+ */
+
 public interface IListing {
     int getListingId();
 
