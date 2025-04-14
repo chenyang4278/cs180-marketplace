2595d761d769a3749bc3b185772b29705967e32a
KauaiMoho <113269300+KauaiMoho@users.noreply.github.com>
2025-04-14 10:11:33 -0400

    planned testcases

--- src/test/server/TestEndpointHandlers.java
+++ src/test/server/TestEndpointHandlers.java
@@ -12,11 +12,51 @@ import static org.junit.Assert.*;
 public class TestEndpointHandlers {
 
     @Test
-    public void testGetUserHandler() {
+    public void testBuyListingHandler() {
+
+    }
+
+    @Test
+    public void testCreateListingHandler() {
+
+    }
+
+    @Test
+    public void testCreateMessageHandler() {
+
+    }
+
+    @Test
+    public void testCreateUserHandler() {
+
+    }
+
+    @Test
+    public void testDeleteListingHandler() {
+
+    }
+
+    @Test
+    public void testDeleteUserHandler() {
+
+    }
+
+    @Test
+    public void testGetListingsFromAttributeHandler() {
+
+    }
+
+    @Test
+    public void testGetMessagesBetweenUsersHandler() {
+
+    }
+
+    @Test
+    public void testGetUserFromIdHandler() {
         DatabaseWrapper db = DatabaseWrapper.get();
         User u = new User("karma", "verysecretpassword");
         try {
-            u.save();
+            DatabaseWrapper.get().save(u);
             GetUserFromIdHandler h = new GetUserFromIdHandler();
             ObjectPacket<User> userP = (ObjectPacket) h.handle(null, new String[] {u.getId() + ""});
             assertEquals(u.getUsername(), userP.getObj().getUsername());
@@ -26,4 +66,8 @@ public class TestEndpointHandlers {
         }
     }
 
+    @Test
+    public void testGetUsersFromAttributeHandler() {
+
+    }
 }
