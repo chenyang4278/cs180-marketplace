f8e64d616258e820ed0e758380d40a5b2c46a7e2
chenyang4278 <chenyang4278@gmail.com>
2025-05-03 13:56:19 -0400

    added interface for view listing popup

--- src/main/client/screens/IViewListingPopup.java
+++ src/main/client/screens/IViewListingPopup.java
@@ -1,5 +1,13 @@
 package client.screens;
 import data.Listing;
+/**
+ * IViewListingPopup
+ *
+ * Interface for ViewListingPopup.
+ *
+ * @author Chen
+ * @version 4/27/25
+ */
 public interface IViewListingPopup {
 
 }
